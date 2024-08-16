e9f4ef94b6094f6dc30d99812502a66e34b2fc31
Rajeevraj1234 <rarajeevraj@gmail.com>
2024-08-16 22:10:25 +0530

    adding shadCN and imporove forntend

--- src/components/landing/Landing.tsx
+++ src/components/landing/Landing.tsx
@@ -1,16 +1,18 @@
-import React from "react";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
 
 const Landing = () => {
   return (
     <div className="h-[100vh] w-[100vw] flex flex-col px-[300px] justify-center items-center">
       <div className="text-[3rem] font-bold">
-        Welcome to the video Transcoder app here to ease you work with us !!
+        Welcome to the video Transcoder 
       </div>
-      <div className="flex  flex-col justify-center items-center">
-        <input type="file" id="video-input"/>
+      <div className="flex  flex-col justify-center items-center my-10 ">
+        <Input id="picture" className="w-[500px] h-[50px] text-[1.6rem]" type="file" />
       </div>
       <div>
-        <button className="border px-4 py-2 border-black font-bold mt-10 rounded-lg bg-yellow-400 text-black">Process</button>
+        <Button>Process</Button>
       </div>
     </div>
   );
