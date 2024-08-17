5e42ea2903c67c2db7f5ed36ab41cfa2d620af24
Rajeevraj1234 <rarajeevraj@gmail.com>
2024-08-17 23:37:42 +0530

    testing the backend

---
+++ src/app/api/user/route.ts
@@ -0,0 +1,3 @@
+export async function GET() {
+  return Response.json({ username: "rajeev", email: "rajeev@gmail.com" });
+}
--- src/components/landing/Landing.tsx
+++ src/components/landing/Landing.tsx
@@ -1,15 +1,29 @@
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
+import axios from "axios";
 
-const Landing = () => {
+async function getUserDetails() {
+  try {
+    const response = await axios.get("http://localhost:3000/api/user");
+    return response.data;
+  } catch (e) {
+    console.log(e);
+  }
+}
+
+const Landing = async () => {
+  const user = JSON.stringify(await getUserDetails())
   return (
     <div className="h-[100vh] w-[100vw] flex flex-col px-[300px] justify-center items-center">
       <div className="text-[3rem] font-bold">
-        Welcome to the video Transcoder 
+        Welcome to the video Transcoder {user}
       </div>
       <div className="flex  flex-col justify-center items-center my-10 ">
-        <Input id="picture" className="w-[500px] h-[50px] text-[1.6rem]" type="file" />
+        <Input
+          id="picture"
+          className="w-[500px] h-[50px] text-[1.6rem]"
+          type="file"
+        />
       </div>
       <div>
         <Button>Process</Button>
