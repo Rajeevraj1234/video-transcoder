e9afac9945b717c394ce417c7a608d754b486283
Rajeevraj1234 <rarajeevraj@gmail.com>
2024-10-02 13:30:39 +0530

    auth fix

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
-module.exports = {
-    env: {
-      NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
-    },
-  };
 
 export default nextConfig;
